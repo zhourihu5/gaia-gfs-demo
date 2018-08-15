4fe56d1e98a8bf7641e87e00b511fd4715e946ed
dan <i@shanhh.com>
2018-08-15 16:09:28 +0800

    feature add demo controller

---
+++ src/main/java/com/longfor/daenerys3/web/demo/Application.java
@@ -0,0 +1,27 @@
+package com.longfor.daenerys3.web.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * @author shanhonghao
+ * @since 2018-08-15 15:20
+ */
+@SpringBootApplication(scanBasePackages = "com.longfor")
+@Slf4j
+public class Application extends SpringApplication {
+
+    public static void main(String[] args) {
+        ConfigurableEnvironment env = SpringApplication.run(Application.class, args).getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "服务端 '{}' 启动完成! \n\t" +
+                        "环境(s): {}\n\t" +
+                        "日志级别: {}" +
+                        "\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getActiveProfiles(),
+                env.getProperty("logging.level.ROOT"));
+    }
+}
---
+++ src/main/java/com/longfor/daenerys3/web/demo/controller/GitController.java
@@ -0,0 +1,31 @@
+package com.longfor.daenerys3.web.demo.controller;
+
+import com.longfor.daenerys3.core.response.BaseResponse;
+import com.longfor.daenerys3.web.core.config.GitVersionConfig;
+import com.longfor.daenerys3.web.core.dto.GitVersionDTO;
+import com.longfor.daenerys3.web.core.mapper.GitVersionMapper;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author shanhonghao
+ * @since 2018-08-15 15:23
+ */
+@RestController
+@RequestMapping("api/demo")
+public class GitController {
+
+    @Resource
+    private GitVersionConfig gitVersionConfig;
+    @Resource
+    private GitVersionMapper gitVersionMapper;
+
+    @GetMapping(value = "git", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public BaseResponse<GitVersionDTO> fetchGitInfo() {
+        return new BaseResponse<>(gitVersionMapper.toDTO(this.gitVersionConfig));
+    }
+}
