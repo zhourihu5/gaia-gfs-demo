ccf17e7559eaa8dd31446f7eb1f1e57a1a2bb678
dan <i@shanhh.com>
2018-08-17 14:56:33 +0800

    feature 支持swagger

--- daenerys3-demo-client/src/main/java/com/longfor/daenerys3/demo/client/EnvDTO.java
+++ daenerys3-demo-client/src/main/java/com/longfor/daenerys3/demo/client/EnvDTO.java
@@ -1,5 +1,7 @@
 package com.longfor.daenerys3.demo.client;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,14 +9,20 @@ import lombok.NoArgsConstructor;
  * @author shanhonghao
  * @since 2018-08-17 11:12
  */
+@ApiModel("env 信息")
 @Data
 @NoArgsConstructor
 public class EnvDTO {
 
+    @ApiModelProperty("自增id")
     private Integer id;
+    @ApiModelProperty("环境名称")
     private String name;
+    @ApiModelProperty("环境描述")
     private String description;
+    @ApiModelProperty("创建者")
     private String createBy;
+    @ApiModelProperty("修改者")
     private String updateBy;
 
 }
--- daenerys3-demo-web/src/main/java/com/longfor/daenerys3/demo/web/controller/EnvController.java
+++ daenerys3-demo-web/src/main/java/com/longfor/daenerys3/demo/web/controller/EnvController.java
@@ -1,8 +1,12 @@
 package com.longfor.daenerys3.demo.web.controller;
 
+import com.github.pagehelper.PageInfo;
 import com.longfor.daenerys3.core.response.BaseResponse;
 import com.longfor.daenerys3.demo.client.EnvDTO;
 import com.longfor.daenerys3.demo.web.service.EnvService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +20,7 @@ import java.util.Optional;
  * @author shanhonghao
  * @since 2018-08-16 18:16
  */
+@Api(value = "env 相关API", tags = {"env"})
 @RestController
 @RequestMapping("api/envs")
 public class EnvController {
@@ -23,21 +28,29 @@ public class EnvController {
     @Resource
     private EnvService envService;
 
+    @ApiOperation(value = "分页获取环境列表", notes = "curl -X GET \"http://127.0.0.1:8080/api/envs?pageNum=0&pageSize=10\" -H \"accept: application/json;charset=UTF-8\"")
     @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public BaseResponse pagianteEnvs(
+    public BaseResponse<PageInfo<EnvDTO>> pagianteEnvs(
+            @ApiParam(value = "当前页码", required = true)
             @Valid @NotNull @Min(0)
             @RequestParam(value = "pageNum") Integer pageNum,
 
+            @ApiParam(value = "分页大小", required = true)
             @Valid @NotNull @Min(1)
-            @RequestParam(value = "pageNum") Integer pageSize,
+            @RequestParam(value = "pageSize") Integer pageSize,
 
+            @ApiParam(value = "环境名称", required = false)
             @RequestParam(value = "name", required = false) String name
     ) {
         return new BaseResponse(envService.paginateEnvs(pageNum, pageSize, name));
     }
 
+    @ApiOperation(value = "查询指定id的环境信息", notes = "curl -X GET \"http://127.0.0.1:8080/api/envs/1\" -H \"accept: application/json;charset=UTF-8\"")
     @GetMapping(value = "{envId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public BaseResponse<EnvDTO> loadEnvById(@PathVariable("envId") Integer envId) {
+    public BaseResponse<EnvDTO> loadEnvById(
+            @ApiParam(value = "环境id", required = true)
+            @Valid @NotNull @Min(1)
+            @PathVariable("envId") Integer envId) {
         Optional<EnvDTO> env = envService.loadEnvById(envId);
         return env.map(BaseResponse::new).orElseGet(BaseResponse::new);
     }
