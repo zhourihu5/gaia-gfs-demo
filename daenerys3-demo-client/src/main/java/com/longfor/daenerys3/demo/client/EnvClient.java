402257520898bc901bfd4db3f3a8f633cb28590e
dan <i@shanhh.com>
2018-08-30 09:54:43 +0800

    feature 使用自建的 feignclient

--- daenerys3-demo-client/src/main/java/com/longfor/daenerys3/demo/client/EnvClient.java
+++ daenerys3-demo-client/src/main/java/com/longfor/daenerys3/demo/client/EnvClient.java
@@ -3,14 +3,16 @@ package com.longfor.daenerys3.demo.client;
 import com.longfor.daenerys3.core.bean.PageInfo;
 import com.longfor.daenerys3.core.response.BaseResponse;
 import com.longfor.daenerys3.demo.client.dto.EnvDTO;
+import com.longfor.daenerys3.web.core.ApiVersion;
+import com.longfor.daenerys3.web.feign.config.GraviteeApiKey;
+import com.longfor.daenerys3.web.feign.config.LFFeignConfiguration;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -18,11 +20,13 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @since 2018-08-16 18:16
  */
 @Api(value = "env 相关API", tags = {"env"})
-@FeignClient("daenerys3-demo-web")
+@FeignClient(value = "daenerys3-demo-web", configuration = LFFeignConfiguration.class)
+@GraviteeApiKey("fuck")
 public interface EnvClient {
 
-    @ApiOperation(value = "分页获取环境列表", notes = "curl -X GET \"http://127.0.0.1:8080/api/envs?pageNum=0&pageSize=10\" -H \"accept: application/json;charset=UTF-8\"")
-    @RequestMapping(value = "v1/api/envs", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiOperation(value = "分页获取环境列表", notes = "curl -X GET \"http://127.0.0.1:8080/v1/api/envs?pageNum=0&pageSize=10\" -H \"accept: application/json;charset=UTF-8\"")
+    @ApiVersion(1)
+    @GetMapping(value = "api/envs", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     BaseResponse<PageInfo<EnvDTO>> pagianteEnvs(
             @ApiParam(value = "当前页码", required = false)
             @RequestParam(value = "pageNum", required = false, defaultValue = "0") Integer pageNum,
@@ -34,8 +38,9 @@ public interface EnvClient {
             @RequestParam(value = "name", required = false) String name
     );
 
-    @ApiOperation(value = "查询指定id的环境信息", notes = "curl -X GET \"http://127.0.0.1:8080/api/envs/1\" -H \"accept: application/json;charset=UTF-8\"")
-    @RequestMapping(value = "v2/api/envs/{envId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiOperation(value = "查询指定id的环境信息", notes = "curl -X GET \"http://127.0.0.1:8080/v2/api/envs/1\" -H \"accept: application/json;charset=UTF-8\"")
+    @ApiVersion(2)
+    @GetMapping(value = "api/envs/{envId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     BaseResponse<EnvDTO> loadEnvById(
             @ApiParam(value = "环境id", required = true)
             @PathVariable("envId") Integer envId
