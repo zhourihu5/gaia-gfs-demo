aec312cae9bb1e6628541238f502c7a031e5f07a
dan <i@shanhh.com>
2018-09-18 15:42:35 +0800

    feature 修改 LFFeignClient 中的 app 为 group

--- gaia-gfs-demo-client/src/main/java/com/longfor/gaia/gfs/demo/client/EnvClient.java
+++ gaia-gfs-demo-client/src/main/java/com/longfor/gaia/gfs/demo/client/EnvClient.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @since 2018-08-16 18:16
  */
 @Api(value = "env 相关API", tags = {"env"})
-@LFFeignClient(value = "gaia-gfs-demo-web", app="gaia-gfs-demo", configuration = LFFeignConfiguration.class)
+@LFFeignClient(value = "gaia-gfs-demo-web", group = "gaia-gfs-demo", configuration = LFFeignConfiguration.class)
 public interface EnvClient {
 
     @ApiOperation(value = "分页获取环境列表", notes = "curl -X GET \"http://127.0.0.1:8080/v1/api/envs?pageNum=0&pageSize=10\" -H \"accept: application/json;charset=UTF-8\"")
