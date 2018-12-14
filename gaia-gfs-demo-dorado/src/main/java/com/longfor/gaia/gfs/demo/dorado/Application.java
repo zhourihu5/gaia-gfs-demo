49b14dea1ec661b5d870f87f2c0d7ff8fabd3147
dan <i@shanhh.com>
2018-12-14 13:54:39 +0800

    feature 完成 dorado demo

--- gaia-gfs-demo-dorado/src/main/java/com/longfor/gaia/gfs/demo/dorado/Application.java
+++ gaia-gfs-demo-dorado/src/main/java/com/longfor/gaia/gfs/demo/dorado/Application.java
@@ -1,30 +1,12 @@
 package com.longfor.gaia.gfs.demo.dorado;
 
-import com.bstek.dorado.web.loader.DoradoLoader;
-import com.longfor.gaia.gfs.web.dorado.config.DoradoApplicationContextInilializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.LinkedHashSet;
-import java.util.Set;
 
 @SpringBootApplication
 public class Application  {
 
-
 	public static void main(String[] args) throws Exception {
-		System.setProperty("doradoHome", "classpath:dorado-home/");
-
-		SpringApplication app = new SpringApplication(Application.class);
-		app.addInitializers(new DoradoApplicationContextInilializer());
-
-		DoradoLoader doradoLoader = DoradoLoader.getInstance();
-		doradoLoader.preload(true);
-
-		Set<Object> sources = new LinkedHashSet<Object>();
-		sources.addAll(doradoLoader.getContextLocations(false));
-		app.setSources(sources);
-
-		app.run(args);
+		SpringApplication.run(Application.class, args);
 	}
 }
